14281e1852b4e88e06ce4b82c63c51d4d6c2facd
Laércio Oliveira <jose.laercio.bezerra@gmail.com>
2020-11-19 22:21:38 -0300

    test: should update total when a product gets included and then removed

--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -7,6 +7,11 @@ describe("Cart", () => {
     price: 35388, // 353.88 | R$ 353,88
   };
 
+  let product2 = {
+    title: "Adidas running shoes - women",
+    price: 41872,
+  };
+
   beforeEach(() => {
     cart = new Cart();
   });
@@ -28,14 +33,32 @@ describe("Cart", () => {
   });
 
   it("should ensure no more than on product exists at a time", () => {
-    add.product({
+    cart.add({
       product,
       quantity: 2,
     });
 
-    add.product({
+    cart.add({
       product,
       quantity: 1,
     });
+
+    expect(cart.getTotal()).toEqual(35388);
+  });
+
+  it("should update total when a product gets included and then removed", () => {
+    cart.add({
+      product,
+      quantity: 2,
+    });
+
+    cart.add({
+      product: product2,
+      quantity: 1,
+    });
+
+    cart.remove(product);
+
+    expect(cart.getTotal()).toEqual(41872);
   });
 });
