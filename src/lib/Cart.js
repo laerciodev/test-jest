09eb7cbe38e9bf0efafe757b5512880444552c6e
Laércio Oliveira <laercio@ar.quei.ro>
2020-11-19 21:19:23 -0300

    chore: should ensure no more than on product exists at a time

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,10 +1,16 @@
 import find from "lodash/find";
-
+import remove from 'lodash/remove';
 export default class Cart {
   items = [];
 
   add(item) {
-    this.items.push(item);
+    const itemToFind = { product: item.product };
+
+    if(find(this.items, itemToFind)) {
+      remove(this.items, itemToFind)
+    }
+
+    this.items.push(item);    
   }
 
   getTotal() {
--- src/lib/Cart.spec.js
+++ src/lib/Cart.spec.js
@@ -2,23 +2,23 @@ import Cart from "./Cart";
 
 describe("Cart", () => {
   let cart;
-  let product;
+  let product = {
+    title: "Adidas running shoes - men",
+    price: 35388, // 353.88 | R$ 353,88
+  };
 
   beforeEach(() => {
     cart = new Cart();
   });
 
-  fit("should return 0 when getTotal() is executed in a newly created instance", () => {
+  it("should return 0 when getTotal() is executed in a newly created instance", () => {
     const cart = new Cart();
     expect(cart.getTotal()).toEqual(0);
   });
 
   it("should multiply quantity and price and receive the total amount", () => {
     const item = {
-      product: {
-        title: "Adidas running shoes - men",
-        price: 35388, // 353.88 | R$ 353,88
-      },
+      product,
       quantity: 2, // 707.76
     };
 
